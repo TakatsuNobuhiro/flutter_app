271873cac7967554bdfd168d1448796b65083b05
高津亘宏 <nobuhiro19980622@gmail.com>
2021-05-24 11:27:41 +0900

    firebase flutter web

--- lib/main.dart
+++ lib/main.dart
@@ -88,7 +88,7 @@ class _MyHomePageState extends State<MyHomePage> {
             Navigator.push(
               context,
               MaterialPageRoute(
-                builder: (context) => NextPage(),
+                builder: (context) => NextPage("高津"),
               ),
             );
           },
--- lib/next_page.dart
+++ lib/next_page.dart
@@ -1,16 +1,15 @@
 import "package:flutter/material.dart";
 
 class NextPage extends StatelessWidget {
+  NextPage(this.name);
+  final String name;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("次の画面"),
-      ),
-      body: Container(
-        height: double.infinity,
-        color: Colors.red,
-      ),
-    );
+        appBar: AppBar(
+          title: Text("次の画面"),
+        ),
+        body: Image.network(
+            "https://futsal.tech/wp-content/uploads/2021/05/4a806ad8e1ed8bf51abb6df94b4e3694-e1621819159858.png"));
   }
 }
